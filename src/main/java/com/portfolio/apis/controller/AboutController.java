96db9d0ed3067b8422498fc2084eaad56dae989c
Jose Maria Gomez <pachomdq@gmail.com>
2022-06-02 14:54:31 -0300

    agregada etiqueta @CrossOrigin

--- src/main/java/com/portfolio/apis/controller/AboutController.java
+++ src/main/java/com/portfolio/apis/controller/AboutController.java
@@ -4,6 +4,7 @@ import com.portfolio.apis.model.About;
 import com.portfolio.apis.service.IAboutService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,10 @@ public class AboutController {
     @Autowired
     private IAboutService interAbout;
     
+    @CrossOrigin(origins = {
+        "https://localhost:4200",
+        "portfolio-5fd97.web.app"
+    })
     @GetMapping("/about")
     public List<About> getAbout(){
         return interAbout.getAbout();
