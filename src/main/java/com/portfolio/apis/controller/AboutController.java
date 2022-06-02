edd132c9d176a61f308d01ef1f8ddc7cc9634b6c
Jose Maria Gomez <pachomdq@gmail.com>
2022-06-02 17:37:26 -0300

    agregada etiqueta @CrossOrigin a todos los controladores

--- src/main/java/com/portfolio/apis/controller/AboutController.java
+++ src/main/java/com/portfolio/apis/controller/AboutController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
 @RestController
 public class AboutController {
     
