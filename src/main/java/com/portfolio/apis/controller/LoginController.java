9e1f93b6fd231a98303e6961dc23a9ecc56fdc29
Jose Maria Gomez <pachomdq@gmail.com>
2022-06-03 16:19:06 -0300

    solucionado cors para login

--- src/main/java/com/portfolio/apis/controller/LoginController.java
+++ src/main/java/com/portfolio/apis/controller/LoginController.java
@@ -2,16 +2,19 @@
 package com.portfolio.apis.controller;
 
 import com.portfolio.apis.model.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 
 @RestController
 public class LoginController {
-    String key = "claveprivada";
+    String key = "JoseMariaGomezArgentinaPrograma";
     @PostMapping("/login")
     public User login(@RequestBody User user){
         
